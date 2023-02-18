34914c8d0dfce12193e55875b660a996bce3b93a
User <michaelm810129@gmail.com>
2023-02-18 23:17:28 +1030

    Added generate html method

---
+++ Structure/structure.js
@@ -0,0 +1,11 @@
+const structure = ` <div class="card w-50">
+<div class="card-body">
+  <h5 class="card-title name">${"Role"}: ${"Name"}</h5>
+  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+  <p class="card-text">${"id"}<p>
+  <p class="card-text">${"email"}<p>
+  <p class="card-text">${"optional github/school"}<p>
+
+  <a href="#" class="btn btn-primary">Button</a>
+</div>
+</div>`
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const { default: inquirer } = require("inquirer");
+const inquirer = require("inquirer");
+const managers = require("./lib/manager");
+const Employee = require("./lib/employee.js");
 
 const option = [
     {name: 'Intern'},
@@ -8,85 +10,113 @@ const option = [
 
 
 
-const questions = inquirer
-.prompt ([
-    {
-        type: 'confirm',
-        message: 'Would you like to create your Team?',
-        default: true,
-        name: 'Team',
+ inquirer
+    .prompt ([
+        {
+            type: 'confirm',
+            message: 'Would you like to create your Team?',
+            default: true,
+            name: 'Team',
 
-    },
-    // Manager Prompts
-    {
-        type: 'input',
-        message: 'Enter Managers name:',
-        name: 'Manager',
-    },
-    {
-        type: 'input',
-        message: 'Enter Managers employee I.D:',
-        name: 'managerId', 
-    },
-    {
-        type: 'input',
-        message: 'Enter Managers Email address:',
-        name: 'mangerEmail',
-    },
-    {
-        type: 'input',
-        message: 'Enter office number:',
-        name: 'mangerOfficeNum',
-    },
-    // Team Selection
-    {
-        type: 'list',
-        message: 'Select Team Member:',
-        name: 'selectedTeam',
-        choices: option,
-    },
-    // Intern Prompts
-    {
-        type: 'input',
-        message: 'Enter Interns name:',
-        name: 'Intern',
-    },
-    {
-        type: 'input',
-        message: 'Enter Interns employee I.D:',
-        name: 'internId', 
-    },
-    {
-        type: 'input',
-        message: 'Enter Interns Email address:',
-        name: 'internEmail',
-    },
-    {
-        type: 'input',
-        message: 'Enter Interns School:',
-        name: 'internSchool',
-    },
-    // Engineer Prompts
-    {
-        type: 'input',
-        message: 'Enter Engineers name:',
-        name: 'engineer',
-    },
-    {
-        type: 'input',
-        message: 'Enter Engineers employee I.D:',
-        name: 'engineersId', 
-    },
-    {
-        type: 'input',
-        message: 'Enter Engineers Email address:',
-        name: 'engineersEmail',
-    },
-    {
-        type: 'input',
-        message: 'Enter GitHub Username:',
-        name: 'engineersGitHub',
-    },
-    
-
+        },
+        // Manager Prompts
+        {
+            type: 'input',
+            message: 'Enter Managers name:',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'Enter Managers employee I.D:',
+            name: 'id', 
+        },
+        {
+            type: 'input',
+            message: 'Enter Managers Email address:',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'Enter office number:',
+            name: 'mangerOfficeNum',
+        },
+        // Team Selection
+        {
+            type: 'list',
+            message: 'Select Team Member:',
+            name: 'selectedTeam',
+            choices: option,
+        },
+        // Intern Prompts
+        {
+            type: 'input',
+            message: 'Enter Interns name:',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'Enter Interns employee I.D:',
+            name: 'internId', 
+        },
+        {
+            type: 'input',
+            message: 'Enter Interns Email address:',
+            name: 'internEmail',
+        },
+        {
+            type: 'input',
+            message: 'Enter Interns School:',
+            name: 'internSchool',
+        },
+        {
+            type: 'list',
+            message: 'Select Team Member:',
+            name: 'selectedTeam',
+            choices: option,
+        },
+        // Engineer Prompts
+        {
+            type: 'input',
+            message: 'Enter Engineers name:',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'Enter Engineers employee I.D:',
+            name: 'engineersId', 
+        },
+        {
+            type: 'input',
+            message: 'Enter Engineers Email address:',
+            name: 'engineersEmail',
+        },
+        {
+            type: 'input',
+            message: 'Enter GitHub Username:',
+            name: 'engineersGitHub',
+        },
+        {
+            type: 'list',
+            message: 'Select Team Member:',
+            name: 'selectedTeam',
+            choices: option,
+        },
 ])
+.then((answers) => {
+//    Creates new employee(manager)
+    const manager = new Employee(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.officeNumb
+    )
+    // Creates HTML using generateCard method/ other roles need extended classes/
+    const managerhtml = manager.generateCard();
+    console.log(managerhtml);
+})
+
+
+
+
+
+module.exports = inquirer;
