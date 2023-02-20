b6a1b5eb4f7b285dbc1badeabbe6a859b589c06c
User <michaelm810129@gmail.com>
2023-02-20 22:24:49 +1030

    added employee functions/refactored code

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 const inquirer = require("inquirer");
-const managers = require("./lib/manager");
-const Employee = require("./lib/employee.js");
-const Engineer = require("./lib/employee.js");
-const Intern = require("./lib/employee.js");
+const {Manager, Engineer, Intern} = require("./lib/manager");
+// const {Engineer} = require("./lib/employee.js");
 
-const option = [
-    {name: 'Intern'},
-    {name: 'Engineer'},
-    {name: 'Finish Building Team'},
-];
-
-
-
- inquirer
+const team = [];
+function generateManager () {
+    inquirer
     .prompt ([
-        {
-            type: 'confirm',
-            message: 'Would you like to create your Team?',
-            default: true,
-            name: 'Team',
-
-        },
+     
         // Manager Prompts
         {
             type: 'input',
@@ -40,42 +26,48 @@ const option = [
         {
             type: 'input',
             message: 'Enter office number:',
-            name: 'mangerOfficeNum',
+            name: 'managerOfficeNum',
         },
-        // Team Selection
+        
+       
+    ])
+       .then((answers) => {
+        const manager = new Manager(
+            answers.name,
+            answers.id,
+            answers.email,
+            answers.managerOfficeNum
+        )
+        team.push(manager);
+        addMember();
+       })
+}
+function addMember () {
+    inquirer.prompt([
         {
             type: 'list',
             message: 'Select Team Member:',
             name: 'selectedTeam',
-            choices: option,
+            choices: ['Engineer', 'Intern', 'Finished selecting Team'],
         },
-        // Intern Prompts
-        {
-            type: 'input',
-            message: 'Enter Interns name:',
-            name: 'name',
-        },
-        {
-            type: 'input',
-            message: 'Enter Interns employee I.D:',
-            name: 'internId', 
-        },
-        {
-            type: 'input',
-            message: 'Enter Interns Email address:',
-            name: 'internEmail',
-        },
-        {
-            type: 'input',
-            message: 'Enter Interns School:',
-            name: 'internSchool',
-        },
-        {
-            type: 'list',
-            message: 'Select Team Member:',
-            name: 'selectedTeam',
-            choices: option,
-        },
+    ])
+    .then((answers) => {
+        if(answers.selectedTeam === 'Engineer') {
+            generateEngineer();
+        }else if(answers.selectedTeam === 'Intern'){
+            generateIntern();
+        }else{
+            // This is where the generate html goes
+            
+            console.log(team);
+        }
+    })
+}
+
+generateManager();
+
+function generateEngineer() {
+    inquirer.prompt([
         // Engineer Prompts
         {
             type: 'input',
@@ -85,49 +77,165 @@ const option = [
         {
             type: 'input',
             message: 'Enter Engineers employee I.D:',
-            name: 'engineersId', 
+            name: 'id',
         },
         {
             type: 'input',
             message: 'Enter Engineers Email address:',
-            name: 'engineersEmail',
+            name: 'email',
         },
         {
             type: 'input',
             message: 'Enter GitHub Username:',
-            name: 'engineersGitHub',
+            name: 'github',
+        },
+    ])
+        .then((engineerAnswers) => {
+            const engineer = new Engineer(
+                engineerAnswers.name,
+                engineerAnswers.id,
+                engineerAnswers.email,
+                engineerAnswers.github,
+            )
+            team.push(engineer);
+            addMember();
+        })
+}
+function generateIntern () {
+    inquirer.prompt ([
+          {
+            type: 'input',
+            message: 'Enter Interns name:',
+            name: 'name',
         },
         {
-            type: 'list',
-            message: 'Select Team Member:',
-            name: 'selectedTeam',
-            choices: ['Engineer', 'Intern', 'Finished selecting Team'],
+            type: 'input',
+            message: 'Enter Interns employee I.D:',
+            name: 'id', 
         },
-])
-.then((answers) => {
-//    Creates new employee(manager)
-    const manager = new managers(
-        answers.name,
-        answers.id,
-        answers.email,
-        answers.officeNumb
-    )
-    // Creates HTML using generateCard method/ other roles need extended classes/
-    const managerhtml = manager.generateCard();
-    console.log("first", managerhtml);
-
-    const engineer = new Engineer(
-        answers.name,
-        answers.id,
-        answers.email,
-        answers.github,
-    )
-    const engineerhtml = engineer.generateEnginCard(Engineer)
-    console.log( "second", engineerhtml);
-})
-
-
+        {
+            type: 'input',
+            message: 'Enter Interns Email address:',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'Enter Interns School:',
+            name: 'school',
+        },
+    ]) .then((answers) => {
+        const intern = new Intern (
+            answers.name,
+            answers.id,
+            answers.email,
+            answers.github,
+        )
+        team.push(intern);
+        addMember();
+    })
+   
+}
 
 
 
-module.exports = inquirer;
+// 
+//        .then((answers) => {
+//         if(answers.selectedTeam === 'Engineer') {
+//             inquirer
+//                 .prompt([
+        
+       
+//         // Engineer Prompts
+//         {
+//             type: 'input',
+//             message: 'Enter Engineers name:',
+//             name: 'name',
+//         },
+//         {
+//             type: 'input',
+//             message: 'Enter Engineers employee I.D:',
+//             name: 'engineersId', 
+//         },
+//         {
+//             type: 'input',
+//             message: 'Enter Engineers Email address:',
+//             name: 'engineersEmail',
+//         },
+//         {
+//             type: 'input',
+//             message: 'Enter GitHub Username:',
+//             name: 'engineersGitHub',
+//         },
+//         {
+//             type: 'list',
+//             message: 'Select Team Member:',
+//             name: 'selectedTeam',
+//             choices: ['Engineer', 'Intern', 'Finished selecting Team'],
+//         },
+//                 ])
+//                 .then((engineerAnswers) => {
+//                     const engineer = new Engineer(
+//                         engineerAnswers.name,
+//                         engineerAnswers.id,
+//                         engineerAnswers.email,
+//                         engineerAnswers.github,
+//                     )
+//                     const engineerhtml = engineer.generateCard()
+//                     console.log( "second", engineerhtml);
+//                 })
+//                 }
+//             })
+        
+       
+// //          // Intern Prompts
+
+//          {
+//             type: 'input',
+//             message: 'Enter Interns name:',
+//             name: 'name',
+//         },
+//         {
+//             type: 'input',
+//             message: 'Enter Interns employee I.D:',
+//             name: 'internId', 
+//         },
+//         {
+//             type: 'input',
+//             message: 'Enter Interns Email address:',
+//             name: 'internEmail',
+//         },
+//         {
+//             type: 'input',
+//             message: 'Enter Interns School:',
+//             name: 'internSchool',
+//         },
+//         {
+//             type: 'list',
+//             message: 'Select Team Member:',
+//             name: 'selectedTeam',
+//             choices: ['Engineer', 'Intern', 'Finished selecting Team'],
+//         },
+// ])
+// .then((answers) => {
+// //    Creates new employee(manager)
+  
+    
+//     // Creates HTML using generateCard method/ other roles need extended classes/
+//     const managerhtml = manager.generateCard();
+//     console.log("first", managerhtml);
+
+//     const engineer = new Engineer(
+//         answers.name,
+//         answers.id,
+//         answers.email,
+//         answers.github,
+//     )
+//     const engineerhtml = engineer.generateCard()
+//     console.log( "second", engineerhtml);
+// })
+
+
+
+
+
+// module.exports = inquirer
