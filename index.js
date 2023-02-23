8149ad1a364c9283cef1bdc990b98b03a0885988
User <michaelm810129@gmail.com>
2023-02-23 17:07:01 +1030

    added css file/Fixed html bug

--- index.js
+++ index.js
@@ -66,14 +66,13 @@ function addMember() {
                 })
                 const cards = htmlArray.join('');
                 const endHtml = `  
-                ${cards}
+${cards}
                 
-             </div>
+    </div>
              
-              <script> src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"</script>
-             </body>
-             
-             </html>`;
+        <script> src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"</script>
+    </body>
+</html>`;
 
 
                 console.log(htmlArray);
